95f776169575a4b55209147bc6a39c06e4698b56
suracI-invert <koolsha123@gmail.com>
2024-03-22 02:51:21 +0700

    add gradio demo

---
+++ fe.py
@@ -0,0 +1,33 @@
+from time import sleep
+
+import gradio as gr
+
+import requests
+
+def query(text):
+    ret = ''
+    res = requests.post('http://127.0.0.1:8000/re/query', json={'text': text})
+    data = res.json()
+    task_id = data['id']
+    while True:
+        res = requests.get(f'http://127.0.0.1:8000/re/query/status/{task_id}')
+        task_state = res.json()
+        if task_state['state'] == 1:
+            docs = [d['content'] for d in task_state['data']['docs']]
+            ret = docs[0]
+            break
+        elif task_state['state'] == 2:
+            ret = 'Failed'
+            break
+        else:
+            sleep(1)
+            continue
+    return ret
+
+demo = gr.Interface(
+    fn=query,
+    inputs=["text"],
+    outputs=["text"],
+)
+
+demo.launch()
